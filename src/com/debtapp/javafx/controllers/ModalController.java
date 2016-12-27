ed6d150b0ea6c9267810b26d3d4b4e20bd6493ec
test <sanek96sm@gmail.com>
2016-12-27 13:54:12 +0200

    Create 4 methods for build modal windows: error window, info window, modal window for editing and modal window for add note. Also create 3 const for path of modal windows above.

--- src/com/debtapp/javafx/controllers/ModalController.java
+++ src/com/debtapp/javafx/controllers/ModalController.java
@@ -1,7 +1,23 @@
 package com.debtapp.javafx.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 /**
  * Created by Arizel on 25.12.2016.
  */
 public class ModalController {
+    @FXML Button btnOk;
+    @FXML Button btnCancel;
+    @FXML TextField fieldName;
+    @FXML TextField fieldPhone;
+    @FXML TextField fieldAddress;
+    @FXML TextField fieldId;
+    @FXML TextField fieldIp;
+
+    public void buttonPressed(ActionEvent actionEvent) {
+
+    }
 }
---
+++ src/com/debtapp/javafx/controllers/ModalControllerError.java
@@ -0,0 +1,10 @@
+package com.debtapp.javafx.controllers;
+
+/**
+ * Created by Arizel on 27.12.2016.
+ */
+public class ModalControllerError {
+    public void errorClose() {
+
+    }
+}
