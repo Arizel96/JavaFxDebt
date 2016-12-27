f65c2e0bd62bd7111202e6da03816513c2e3a717
test <sanek96sm@gmail.com>
2016-12-27 22:45:09 +0200

    Controller for modal with some info about errors with operations. Have variable "info" for displaying in label.

--- src/com/debtapp/javafx/controllers/ModalControllerError.java
+++ src/com/debtapp/javafx/controllers/ModalControllerError.java
@@ -1,10 +1,47 @@
 package com.debtapp.javafx.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Arizel on 27.12.2016.
  */
-public class ModalControllerError {
-    public void errorClose() {
+public class ModalControllerError implements Initializable{
+    private Button clickedButton;
+    private Stage stage;
+    private static String info = "";
 
+    @FXML Button btnOkeyError;
+    @FXML Label lblError;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        lblError.setText(info);
+    }
+
+    public void errorClose(ActionEvent actionEvent) {
+        clickedButton = (Button) actionEvent.getSource();
+        switch (clickedButton.getId()) {
+            case "btnOkeyError" :
+                stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.close();
+                break;
+            case "btnCancel" :
+                stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.close();
+                break;
+        }
+    }
+
+    public static void setInfo(String info) {
+        ModalControllerError.info = info;
     }
 }
