32fd28a566a47e47d73c01ef08ac814d81ebe522
test <sanek96sm@gmail.com>
2016-12-27 22:46:17 +0200

    Nothing new, just 3 textfields.

--- src/com/debtapp/javafx/controllers/ModalController.java
+++ src/com/debtapp/javafx/controllers/ModalController.java
@@ -2,8 +2,10 @@ package com.debtapp.javafx.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * Created by Arizel on 25.12.2016.
@@ -17,7 +19,25 @@ public class ModalController {
     @FXML TextField fieldId;
     @FXML TextField fieldIp;
 
-    public void buttonPressed(ActionEvent actionEvent) {
+    private Button clickedButton;
+    private Stage stage;
 
+    public void buttonPressed(ActionEvent actionEvent) {
+        clickedButton = (Button) actionEvent.getSource();
+        switch (clickedButton.getId()) {
+            case "btnOk" :
+                if (checkEmpty()) {
+                    Controller.modalCreator.createErrorModal(actionEvent, Controller.ERROR_PATH, "Заполните все пустые поля!");
+                }
+                break;
+            case "btnCancel" :
+                stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.close();
+                break;
+        }
+    }
+
+    private boolean checkEmpty() {
+        return true;
     }
 }
